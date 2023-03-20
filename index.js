a78626a03da0c231798b0dec1baeef5834b30619
tonantr <tonantruong@hotmail.com>
2023-03-19 23:34:05 -0400

    complete Advanced Functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (arr) => arr.slice(0, 2);
+
+const returnLastTwoDrivers = (arr) => arr.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    return function(fare) {return num * fare};
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arr, cb) {
+    return cb(arr);
+};
